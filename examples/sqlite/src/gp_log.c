e84b424d39f43f8d046a2d87f9e5d907faa6c0a1
Peter Malmberg <peter.malmberg@gmail.com>
2016-03-03 15:01:35 +0100

    Added sqlite

---
+++ examples/sqlite/src/gp_log.c
@@ -0,0 +1,164 @@
+/**
+ *---------------------------------------------------------------------------
+ * @file    gp_log.c
+ * @brief   A general purpose log/error management library.
+ *
+ * @author  Peter Malmberg <peter.malmberg@gmail.com>
+ * @date    2015-04-14
+ * @licence GPLv2
+ *
+ *---------------------------------------------------------------------------
+ */
+
+#include <syslog.h>
+#include <stdio.h>
+#include <glib-2.0/glib.h>
+#include <glib/gstdio.h>
+
+#include "Def.h"
+#include "gp_log.h"
+
+/**
+ * Macro declarations
+ *------------------------------------------------------------------
+ */
+
+
+/**
+ * Variable declarations
+ *------------------------------------------------------------------
+ */
+
+gboolean gp_verbose=FALSE;
+gboolean gp_syslog=FALSE;
+//gboolean gp_debug=FALSE;
+gboolean gp_noColor=FALSE;
+
+int gp_maxSize;
+int gp_logSize;
+
+char *gp_logFile;
+char *gp_logDir;
+
+FILE *gpLogFile = NULL;
+char *gpLogFilename;
+
+int log_mask;
+unsigned int verbose_mask;
+
+static void gp_log_handler(const gchar *log_domain,
+                   GLogLevelFlags log_level,
+                   const gchar *message,
+                   gpointer user_data );
+
+/**
+ * Code
+ *------------------------------------------------------------------
+ */
+
+void gp_log_set_verbose(gboolean v) {
+  gp_verbose = v;
+}
+
+void gp_log_set_verbose_mask(int mask) {
+	verbose_mask = mask;
+}
+
+void gp_log_set_log_mask(int mask) {
+	log_mask = mask;
+}
+
+
+
+void gp_log_init(char *logfile) {
+	GStatBuf st;
+	
+	gpLogFilename = logfile;
+	
+	gp_log_set_verbose(FALSE);
+	
+	gp_log_set_verbose_mask(GP_ALL);
+	
+	gp_log_set_log_mask(GP_ALL);
+	
+	/* Set handler for all levels */
+  g_log_set_handler(G_LOG_DOMAIN, G_LOG_LEVEL_MASK | G_LOG_LEVEL_ERROR | G_LOG_FLAG_FATAL, gp_log_handler, NULL);
+	
+	gpLogFile = g_fopen(logfile, "a");
+  if (gpLogFile==NULL) {
+    g_warning("Could not open file for message logging\n");
+		return;
+  }
+	
+  g_stat(gpLogFilename, &st);
+	gp_logSize = st.st_size;
+	g_debug("Logfile size: %d\n", gp_logSize);
+
+}
+
+void gp_log_close() {
+  fclose(gpLogFile);  
+}
+
+static void gp_log_handler(const gchar *log_domain,
+                           GLogLevelFlags log_level,
+                           const gchar *message,
+                           gpointer user_data )
+{
+	char *color;
+  char *level;
+  GDateTime *dt;
+  char *dateTime;
+	char *eDate;
+	char *eTime;
+	int i;
+  int lm,vm;
+
+  color = E_WHITE;
+  level = "    ";
+  switch (log_level & G_LOG_LEVEL_MASK) {
+	 case G_LOG_LEVEL_WARNING:  color = E_BR_YELLOW;  level = "warn"; lm = log_mask & GP_WARNING;  vm = verbose_mask & GP_WARNING;  break;
+	 case G_LOG_LEVEL_ERROR:    color = E_BR_RED;     level = "err "; lm = log_mask & GP_ERROR;    vm = verbose_mask & GP_ERROR;    break;
+	 case G_LOG_LEVEL_CRITICAL: color = E_WONR;       level = "crit"; lm = log_mask & GP_CRITICAL; vm = verbose_mask & GP_CRITICAL; break;
+	 case G_LOG_LEVEL_DEBUG:    color = E_BR_GREEN;   level = "dbg "; lm = log_mask & GP_DEBUG;    vm = verbose_mask & GP_DEBUG;    break;
+	 case G_LOG_LEVEL_INFO:     color = E_WHITE;      level = "info"; lm = log_mask & GP_INFO;     vm = verbose_mask & GP_INFO;     break;
+	 case G_LOG_LEVEL_MESSAGE:  color = E_BR_CYAN;    level = "msg "; lm = log_mask & GP_MESSAGE;  vm = verbose_mask & GP_MESSAGE;  break;
+   default: color = E_WHITE; level="dflt"; break; 
+  }
+  
+  // date and time information
+  dt = g_date_time_new_now_local();
+  dateTime = g_date_time_format(dt,"%Y-%m-%d %k:%M:%S");
+  eTime = g_date_time_format(dt,"%k:%M:%S");
+	eDate = g_date_time_format(dt,"%Y-%m-%d");
+  
+	
+  // print to stdout if in verbose mode
+  if ( gp_verbose && vm ) {
+		if (SHOW_DATE()) {
+		  printf("%s ", eDate);
+		}
+		if (SHOW_TIME()) {
+		  printf("%s ", eTime);
+		}
+    printf("[%s%s%s] %s",color,level,E_END,message);
+  }
+  
+  if (gp_logSize>gp_maxSize) {
+  	//g_see
+  }
+
+  // write to logfile if existing
+  if (gpLogFile!=NULL && lm) {
+    i = fprintf(gpLogFile, "%s [%s] %s", dateTime, level, message);
+    if (i>0)
+    	gp_logSize += i;
+  }
+  g_free(dateTime);
+  
+  return ;
+}
+
+void gp_log_print() {
+
+}
---
+++ examples/sqlite/src/gp_log.h
@@ -0,0 +1,89 @@
+/**
+ *---------------------------------------------------------------------------
+ * @file    gp_log.h
+ * @brief   A general purpose log/error management library.
+ *
+ * @author  Peter Malmberg <peter.malmberg@gmail.com>
+ * @date    2015-04-14
+ * @licence GPLv2
+ *
+ *---------------------------------------------------------------------------
+ */
+#ifndef GP_LOG_H
+#define	GP_LOG_H
+
+#include <glib-2.0/glib.h>
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+/**
+ * Macro declarations
+ *------------------------------------------------------------------
+ */
+/*
+	#define gp_log_message()
+	#define gp_log_debug()
+  #define gp_log_warning()
+  #define gp_log_error()
+	#define gp_log_critical()
+*/
+#define GP_MESSAGE   0x0001
+#define GP_DEBUG     0x0002
+#define GP_WARNING	 0x0004
+#define GP_ERROR	   0x0008
+#define GP_CRITICAL  0x0010
+#define GP_INFO      0x0020
+#define GP_TIME      0x0040
+#define GP_DATE      0x0080
+
+#define GP_ALL (GP_MESSAGE | GP_DEBUG | GP_WARNING | GP_ERROR | GP_CRITICAL | GP_INFO)
+
+#define SHOW_DATE()  (verbose_mask & GP_DATE) 
+#define SHOW_TIME()  (verbose_mask & GP_TIME)
+
+	
+/**
+ * Variable declarations
+ *------------------------------------------------------------------
+ */
+
+
+/**
+ * Function declarations
+ *------------------------------------------------------------------
+ */
+	
+/**
+ * Init log system.
+ */
+void gp_log_init(char *logfile);
+
+void gp_log_set_verbose_mask(int mask);
+	
+void gp_log_set_log_mask(int mask);
+	
+/**
+ *  Set verbose mode
+ */
+void gp_log_set_verbose(gboolean v);
+
+/**
+ * Print log to stdout.
+ */
+void gp_log_print();
+  
+  	
+/**
+ * Close down log system
+ */
+void gp_log_close();
+
+	
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* GP_LOG_H */
+
