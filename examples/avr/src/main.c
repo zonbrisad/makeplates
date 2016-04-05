a2a7b4bd108b9f66143f2ff662cf8e5c83f6ddd5
Peter Malmberg <peter.malmberg@gmail.com>
2016-04-05 02:03:21 +0200

    Fixed linker error Added simulavr function

--- examples/avr/src/main.c
+++ examples/avr/src/main.c
@@ -1,7 +1,46 @@
 
 #include <stdio.h>
+#include <util/delay.h>
+
+
+/* This port correponds to the "-W 0x20,-" command line option. */
+#define special_output_port (*((volatile char *)0x20))
+
+/* This port correponds to the "-R 0x22,-" command line option. */
+#define special_input_port  (*((volatile char *)0x22))
+
+
+static int uart_putchar(char c, FILE *stream);
+
+static FILE mystdout = FDEV_SETUP_STREAM(uart_putchar, NULL, _FDEV_SETUP_WRITE);
+
+static int uart_putchar(char c, FILE *stream) {
+	special_output_port = c;
+	return 0;
+}
+
+
+/*
+ *  *  Poll the specified string out the debug port.
+ *  */
+void debug_puts(const char *str)
+{
+	  const char *c;
+	
+	  for ( c=str ; *c ; c++ )
+		    special_output_port = *c;
+}
+
 
 int main() {
+
+	stdout = &mystdout;
+//	debug_puts("Test\n");
+	printf("Bashplate AVR example\n");
+	while (1) {
+		special_output_port = 'K';
+		_delay_ms(200);
+	}
 
   return 0;
 }
