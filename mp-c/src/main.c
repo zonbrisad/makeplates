f9054b70d20a0be1a5dfdecdeefcfd0c8990a9fe
Peter Malmberg <peter.malmberg@gmail.com>
2016-10-12 09:57:45 +0200

    Improved def.h

--- mp-c/src/main.c
+++ mp-c/src/main.c
@@ -3,7 +3,7 @@
 #include "def.h"
 
 int main(int argc, char *argv[]) {
-
+	int i;
 	printf("Makeplate c example.\n");
 
 	INFOPRINT("Information printout\n");
@@ -11,6 +11,19 @@ int main(int argc, char *argv[]) {
 	DEBUGPRINT("Debug printout\n");
 
 	DEBUG_DO(printf("Debug do\n"));
+	
+	
+	i = 0;
+	
+	printf("Var i = %2x\n", i);
+	BIT_SET(i, 4);
+	printf("Var i = %2x\n", i);
+	BIT_SET(i, 2);
+	printf("Var i = %2x\n", i);
+	BIT_CLEAR(i,4);
+	printf("Var i = %2x\n", i);
+	
+	
 	return 0;
 
 }
