84924b6f7c691dfec99b01cb2b7d5143864d7bea
Peter <peter.malmberg@gmail.com>
2017-04-13 17:44:02 +0200

    A

--- examples/avr/src/main.c
+++ examples/avr/src/main.c
@@ -15,17 +15,24 @@
 #include <stdio.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
-
+#include <avr/wdt.h>
+#include <avr/pgmspace.h>
+#include <avr/io.h>
+	
 #include "def.h"
+#include "def_avr.h"
+#include "uart.h"
 #include "avrsimul.h"
 
 // Macros -----------------------------------------------------------------
 
 #define PROGNAME "makeplate"
-
+#define VERSION  "0.1"
+#define UART_BAUD_RATE 57600
 
 // Variables --------------------------------------------------------------
-static FILE mystdout = FDEV_SETUP_STREAM(simul_putchar, NULL, _FDEV_SETUP_WRITE);
+// static FILE mystdout = FDEV_SETUP_STREAM(simul_putchar, NULL, _FDEV_SETUP_WRITE);
+static FILE mystdout = FDEV_SETUP_STREAM(uart_putc, uart_getc, _FDEV_SETUP_WRITE);
 
 volatile int timer2_ticks;
 
@@ -40,7 +47,13 @@ ISR(TIMER2_COMPA_vect) {
 }
 
 void hw_init(void) {
-	stdout = &mystdout;
+	//wdt_disable();                            // disable watchdog timer
+	stdout = &mystdout;                       // configure stdout to UART 0
+	
+	ARD_LED_INIT();
+	ARD_LED_ON();
+	
+	uart_init(UART_BAUD_SELECT(UART_BAUD_RATE,F_CPU));      // init uart
 	
 	timer2_ticks = 0;
 	/* Set up our timers and enable interrupts */
@@ -51,17 +64,22 @@ void hw_init(void) {
 	sei();
 }
 
+int kalle(int a, char b);
+
+
 int main(void) {
 	volatile int tmp;
 	
 	hw_init();
 	
 	printf("Makeplate AVR example\n");
+	
 	while (1) {
 		tmp = timer2_ticks;
 		printf("Timer counter %d\n", tmp);
 //		printf("Input char %c\n", simul_getchar());
-		_delay_ms(200);
+		_delay_ms(100);
+		ARD_LED_TOGGLE();
 	}
 
   return 0;
