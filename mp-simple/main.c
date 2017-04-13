47c76f7862bdb6ac9ced76974bd93fdd53d11b3f
Peter <peter.malmberg@gmail.com>
2017-04-13 14:53:25 +0200

    fixed simple example

--- mp-simple/main.c
+++ mp-simple/main.c
@@ -1,11 +1,11 @@
 /**
  *---------------------------------------------------------------------------
- * @brief   Makeplates main example file.
+ * @brief   Makeplates simple example file.
  *
  * @file    main.c
  * @author  Peter Malmberg <peter.malmberg@gmail.com>
  * @date    2016-10-24
- * @licence GPLv2
+ * @licence MIT
  *
  *---------------------------------------------------------------------------
  */
@@ -20,8 +20,6 @@
 #include <string.h>
 #include <errno.h>
 
-#include "def.h"
-
 // Defines ----------------------------------------------------------------
 
 #define PROGNAME "makeplate"
@@ -29,61 +27,13 @@
 // Variables --------------------------------------------------------------
 
 // Prototypes -------------------------------------------------------------
-void sigInt(int sig);
-void sigHup(int sig);
 
 // Code -------------------------------------------------------------------
 
 
-void sigInt(int sig) {
-	UNUSED(sig);
-	
-	printf("\nExiting program\n");
-	exit(0);
-}
-
-void sigHup(int sig) {
-	UNUSED(sig);
-	printf("Sighup\n");
-}
-
-
 int main(int argc, char *argv[]) {
-	int i;
-	
-	UNUSED(argc);
-	UNUSED(argv);
-	
-	signal(SIGINT, sigInt);
-	signal(SIGHUP, sigHup);
-	
-	printf("\nMakeplate linux C example.\n\n\n");
 
-	INFOPRINT("Information printout\n");
-	
-	DEBUGPRINT("Debug printout\n");
-	
-	WARNINGPRINT("Warning printout\n");
-	
-	ERRORPRINT("Error printout\n");
-	
-	DEBUG_DO(printf("Debug do\n"));
-	
-	
-	i = 0;
-	
-	printf("Var i = %2x\n", i);
-	BIT_SET(i, 4);
-	printf("Var i = %2x\n", i);
-	BIT_SET(i, 2);
-	printf("Var i = %2x\n", i);
-	BIT_CLEAR(i,4);
-	printf("Var i = %2x\n", i);
+	printf("Simple example\n");	
 
-	
-	printf("Use CTRL-C to stop program\n");
-	while(1) {
-	}
-	
 	return 0;
 }
