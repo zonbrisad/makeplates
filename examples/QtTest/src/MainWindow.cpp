3909328982dd14b266536ccf3312ab70415c75f2
Peter <peter.malmberg@gmail.com>
2017-11-03 15:39:14 +0100

    Added examples to qtproject

--- examples/QtTest/src/MainWindow.cpp
+++ examples/QtTest/src/MainWindow.cpp
@@ -1,37 +1,89 @@
 
 #include <QMainWindow>
+#include <QtWidgets>
 #include <QTextEdit>
 #include <QHBoxLayout>
 #include <QVBoxLayout>
 #include <QPushButton>
+#include <QSpacerItem>
+#include <QMenuBar>
+#include <QComboBox>
 
 #include "MainWindow.h"
 
 
 MainWindow::MainWindow() {
-	setWindowTitle("Kallexxx");
-	
+
+	// Set window size.
+    resize(MainWindow_XSIZE, MainWindow_YSIZE);
+
+    // Set window title
+	setWindowTitle(MainWindow_TITLE);
+
 	QWidget *window = new QWidget;
-	
 	QTextEdit *textEdit = new QTextEdit(this);
-	//setCentralWidget(textEdit);
-	QHBoxLayout *hLayout = new QHBoxLayout();
-	QVBoxLayout *vLayout = new QVBoxLayout();
+
+	QHBoxLayout *hLayout  = new QHBoxLayout();
+	hLayout->setSpacing(MainWindow_Spacing);
+	hLayout->setContentsMargins(MainWindow_LeftMarigin, MainWindow_RightMarigin,
+				                MainWindow_TopMarigin,  MainWindow_BottomMarigin);
+
+	QVBoxLayout *vLayout  = new QVBoxLayout();
+	vLayout->setSpacing(MainWindow_Spacing);
+	vLayout->setContentsMargins(MainWindow_LeftMarigin, MainWindow_RightMarigin,
+			                    MainWindow_TopMarigin,  MainWindow_BottomMarigin);
 	
-	QPushButton *button1 = new QPushButton("B1");
-	QPushButton *button2 = new QPushButton("B2");
-	QPushButton *button3 = new QPushButton("B3");
+	QPushButton *button1  = new QPushButton("B1");
+	connect(button1, SIGNAL (clicked()),this, SLOT (button1_clicked()));
+
+	QPushButton *button2  = new QPushButton("B2");
+	QPushButton *button3  = new QPushButton("B3");
 	
 	vLayout->addWidget(button1);
 	vLayout->addWidget(button2);
 	vLayout->addWidget(button3);
 	
+
+    QComboBox *combo1 = new QComboBox();
+    combo1->addItem("Item a", 101);
+    combo1->addItem("Item b", 102);
+    combo1->addItem("Item c", 103);
+
+    connect(combo1, SIGNAL (activated()),this, SLOT (combo1Handler()));
+    //connect(combo1, SIGNAL (currentIndexChanged()),this, SLOT (combo1Handler()));
+
+    vLayout->addWidget(combo1);
+
+
+	// Spacer for left side panel
+	vLayout->addItem(new QSpacerItem(20, 20, QSizePolicy::Expanding, QSizePolicy::Expanding));
+
 	hLayout->addLayout(vLayout);
 	hLayout->addWidget(textEdit);
 	
-	window->setLayout(hLayout);
+	// Statusbar
+    QStatusBar *statusbar = new QStatusBar();
+    statusbar->setLayoutDirection(Qt::RightToLeft);
+    setStatusBar(statusbar);
+
+    // Menubar
+    QMenuBar *menubar = new QMenuBar();
+  //  menubar->setGeometry(new QRect(0, 0, 855, 25));
+    setMenuBar(menubar);
+
 	
-	//setLayout(hLayout);
+
+	window->setLayout(hLayout);
 	setCentralWidget(window);
 	
+}
+
+
+void MainWindow::button1_clicked() {
+	qDebug() << "Button 1 clicked\n";
+}
+
+
+void MainWindow::combo1Handler(int a) {
+	qDebug() << "Combo 1handler\n";
 }
--- examples/QtTest/src/MainWindow.h
+++ examples/QtTest/src/MainWindow.h
@@ -1,4 +1,7 @@
 
+// http://www.bogotobogo.com/Qt/Qt5_QStatusBar.php
+
+
 #include <QMainWindow>
 
 class QAction;
@@ -7,13 +10,28 @@ class QPlainTextEdit;
 class QSessionManager;
 
 
+#define MainWindow_TITLE "Nisse"
+#define MainWindow_XSIZE 500
+#define MainWindow_YSIZE 400
+
+#define MainWindow_Spacing       2
+
+#define MainWindow_LeftMarigin   2
+#define MainWindow_RightMarigin  2
+#define MainWindow_TopMarigin    2
+#define MainWindow_BottomMarigin 2
+
+
+
 class MainWindow : public QMainWindow {
 	    Q_OBJECT
 		 
 	public:
 		 explicit MainWindow();
 
-	private slots:	 
+	private slots:
+	void button1_clicked();
+	void combo1Handler(int a);
 		 
 	private: 
 		 QPlainTextEdit *textEdit;
