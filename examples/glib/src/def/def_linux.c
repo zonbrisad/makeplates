bec6e90bd9065ff8dcc71698b6aa6b3eee0fd53d
Peter <peter.malmberg@gmail.com>
2017-12-06 16:54:34 +0100

    Added new def

---
+++ examples/glib/src/def/def_linux.c
@@ -0,0 +1,188 @@
+/**
+ *---------------------------------------------------------------------------
+ * @brief    Linux utils
+ *
+ * @file     def_linux.c
+ * @author
+ * @date     2017-10-22
+ * @license
+ *
+ *---------------------------------------------------------------------------
+ *
+ *
+ */
+// Includes ---------------------------------------------------------------
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <errno.h>
+#include <signal.h>
+
+#include "def.h"
+#include "def_linux.h"
+
+// Macros -----------------------------------------------------------------
+
+// Variables --------------------------------------------------------------
+
+// Prototypes -------------------------------------------------------------
+
+// Code -------------------------------------------------------------------
+
+void printSysInfo(void) {
+    //  defprintf("File:       "__FILE__"\n");
+    defprintf("Build:      "__DATE__"  "__TIME__"\n");
+    defprintf("C Standard: "STRINGIZE(__STDC_VERSION__)"\n");
+#ifdef __GNUC__
+    defprintf("GNU C ver:  "__GNUC_VERSION__"\n");
+#endif
+
+    //defprintf("Byteorder:  "STRINGIZE(__BYTE_ORDER)"\n");
+#ifdef __BIG_ENDIAN__
+    defprintf("Byteorder:  big endian\n");
+#endif
+#ifdef __LITTLE_ENDIAN__
+    defprintf("Byteorder:  little endian\n");
+#endif
+
+#ifdef __cplusplus
+    defprint("C++: enabled\n");
+#endif
+
+}
+
+
+char *getPathToSelf(void) {
+    static char buf[128];
+    int size;
+
+    size = readlink("/proc/self/exe", buf, 128);
+    buf[size] = '\0';
+    return buf;
+}
+
+void removePidFile(char *pidFile) {
+    remove(pidFile);
+}
+
+/**
+ * http://www.man7.org/tlpi/code/online/dist/filelock/create_pid_file.c.html
+ */
+int createPidFile(char *pidFile) {
+    pid_t pid, fPid;
+    int  e;
+    FILE *file;
+
+    DEBUGPRINT("Creating PID file. (%s)\n", pidFile);
+
+    // get pid of this process
+    pid = getpid();
+
+    DEBUGPRINT("This process PID=%d\n", pid);
+
+    // open pid file and read PID
+    file = fopen(pidFile, "a+");
+
+    if ( file == NULL ) {
+        ERRORPRINT("Failed to open pidfile (%s) [%s]\n", pidFile, strerror(errno));
+        return -1;
+    } else {
+
+        e = fscanf(file, "%d", &fPid);
+
+        // if pid file exist check if process is running
+        if (e > 0) {
+
+        }
+
+        if (e == -1) {
+            goto writePid;
+        } else {
+            WARNINGPRINT("Found PID file %d\n", fPid);
+        }
+
+        // check if process is running
+        e = kill(fPid, 0);
+
+        // write new pid to file
+writePid:
+        rewind(file);
+        fprintf(file, "%d\n", pid);
+        //closePid:
+        fclose(file);
+    }
+
+    return 0;
+}
+
+
+
+void daemonize(void) {
+    pid_t pid, sid;
+    int fd;
+
+    /* already a daemon */
+    if ( getppid() == 1 ) {
+        return;
+    }
+
+    /* Fork off the parent process */
+    pid = fork();
+
+    if (pid < 0) {
+        exit(EXIT_FAILURE);
+    }
+
+    /* Killing the Parent Process*/
+    if (pid > 0) {
+        exit(EXIT_SUCCESS);
+    }
+
+    /* At this point we are executing as the child process */
+
+    /* Create a new SID for the child process */
+    sid = setsid();
+
+    if (sid < 0) {
+        exit(EXIT_FAILURE);
+    }
+
+    /* Change the current working directory. */
+    if ((chdir("/")) < 0)   {
+        exit(EXIT_FAILURE);
+    }
+
+    fd = open("/dev/null", O_RDWR, 0);
+
+    // redirect streams
+    if (fd != -1) {
+        dup2 (fd, STDIN_FILENO);
+        dup2 (fd, STDOUT_FILENO);
+        dup2 (fd, STDERR_FILENO);
+
+        if (fd > 2) {
+            close (fd);
+        }
+    }
+
+    /* resettign File Creation Mask */
+    umask(027);
+}
+
+void terminalSize(int *cols, int *lines) {
+	struct winsize ts;
+	*cols  = 0;
+	*lines = 0;
+
+	ioctl(STDIN_FILENO,  TIOCGWINSZ , &ts);
+	*cols = ts.ws_col;
+	*lines = ts.ws_row;
+	//printf("Terminal is %dx%d\n", cols, lines);
+}
---
+++ examples/glib/src/def/def_linux.h
@@ -0,0 +1,70 @@
+/**
+ *---------------------------------------------------------------------------
+ * @brief    Linux utils
+ *
+ * @file     def_linux.h
+ * @author   
+ * @date     2017-10-22
+ * @license  
+ *
+ *---------------------------------------------------------------------------
+ *
+ *
+ */
+#ifndef DEF_LINUX_H
+#define DEF_LINUX_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Includes ---------------------------------------------------------------
+
+// Macros -----------------------------------------------------------------
+
+// Typedefs ---------------------------------------------------------------
+
+// Variables --------------------------------------------------------------
+
+// Prototypes -------------------------------------------------------------
+
+
+/**
+ * Print system information.
+ */
+void printSysInfo(void);
+
+/**
+ *
+ * @return
+ */
+char *getPathToSelf(void);
+
+/**
+ *
+ * @param pidFile
+ * @return
+ */
+int createPidFile(char *pidFile);
+
+/**
+ *
+ * @param pidFile
+ */
+void removePidFile(char *pidFile);
+
+
+/**
+ * Start process in daemon mode.
+ */
+void daemonize(void);
+
+
+	
+void terminalSize(int *cols, int *lines);
+	
+#ifdef __cplusplus
+} //end brace for extern "C"
+#endif
+#endif
+
